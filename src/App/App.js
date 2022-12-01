cd86bd87420333391ab1e5e4fd4d26ff006d1a01
James Wasmer <jameswasmer@gmail.com>
2022-12-01 16:46:21 -0500

    Delete boilerplate Create React App code

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      detailViewId: null,
+      movies: []
+    }
+  }
+
+  
+}
+
+export default App;
