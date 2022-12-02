9175d70e744378648c93d74d658265fd43c80664
James Wasmer <jameswasmer@gmail.com>
2022-12-02 16:54:05 -0500

    Deconstruct movie object props

--- src/DetailView/DetailView.js
+++ src/DetailView/DetailView.js
@@ -1,10 +1,27 @@
 import React from "react";
+import AllMoviesView from "../AllMoviesView/AllMoviesView";
 import "./DetailView.css"
 
-function DetailView({ movie, closeMovie }) {
-  return <DetailView
-  
-    />
+function DetailView({
+  id,
+  title,
+  poster_path,
+  backdrop_path,
+  release_date,
+  overview,
+  average_rating,
+  genres,
+  name,
+  budget,
+  revenue,
+  runtime,
+  tagline,
+  closeMovie }) {
+
+  return <section className="detail-view">
+    <img src={poster_path} alt={`A poster featuring art for the movie ${title}`}/>
+    
+  </section>
 }
 
 export default DetailView
