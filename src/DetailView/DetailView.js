da0df5fda420dcc4a3b9ab8f72d9c43c76d55a7d
James Wasmer <jameswasmer@gmail.com>
2022-12-03 18:47:32 -0500

    Build basic styling and add video placeholder

--- src/DetailView/DetailView.js
+++ src/DetailView/DetailView.js
@@ -25,6 +25,7 @@ class DetailView extends Component {
           overview: data.overview,
           posterPath: data.poster_path,
           releaseDate: data.release_date,
+          releaseYear: data.release_date.slice(0, 4),
           revenue: data.revenue,
           runtime: data.runtime,
           tagline: data.tagline,
@@ -35,18 +36,22 @@ class DetailView extends Component {
 
   render() {
     return <>
-      <p>{this.movieId}</p>
-      <p>{this.state.averageRating}</p>
-      <img className="backdrop" src={this.state.backdropPath} alt={`Artwork for the movie ${this.state.title}`}/>
-      <p>{this.state.budget}</p>
-      <p>{this.state.genres}</p>
-      <p>{this.state.overview}</p>
-      <img className="poster" src={this.state.posterPath} alt={`Artwork for the movie ${this.state.title}`}/>
-      <p>{this.state.releaseDate}</p>
-      <p>{this.state.revenue}</p>
-      <p>{this.state.runtime}</p>
-      <p>{this.state.tagline}</p>
-      <p>{this.state.title}</p>
+      <section className="details-container" style={{backgroundImage: `linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.75) 50%, rgba(0,0,0,1) 100%),
+url(${this.state.backdropPath})`}}>
+        <img className="poster" src={this.state.posterPath} alt={`Artwork for the movie ${this.state.title}`}/>
+        <div className="text-container">
+          <h2 className="title">{`${this.state.title} (${this.state.releaseYear})`}</h2>
+          <h3 className="genres">{this.state.genres}</h3>
+          <p className="tagline">{this.state.tagline}</p>
+          <p className="rating">{this.state.averageRating}</p>
+          <p className="overview">{this.state.overview}</p>
+          <p className="movie-info">{this.state.runtime}</p>
+          <p className="movie-info">{this.state.releaseDate}</p>
+          <p className="movie-info">{this.state.budget}</p>
+          <p className="movie-info">{this.state.revenue}</p>
+        </div>
+      </section>
+      <iframe className="video-embed" src="https://www.youtube.com/embed/SUXWAEX2jlg" title="Fight Club Trailer - HD" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <button onClick={() => {this.closeMovie()}}>Close Movie</button>
     </>
   }
