85ccdfbd8d61d8f2aa83b33269ac48accc82d806
James Wasmer <jameswasmer@gmail.com>
2022-12-03 20:04:38 -0500

    Reformat API date object into display string

--- src/DetailView/DetailView.js
+++ src/DetailView/DetailView.js
@@ -1,3 +1,4 @@
+import { keyboard } from "@testing-library/user-event/dist/keyboard"
 import React, { Component } from "react"
 import "./DetailView.css"
 
@@ -15,6 +16,8 @@ class DetailView extends Component {
       .then(response => response.json())
       .then(movieObject => {
         const data = movieObject.movie
+        const releaseDateObject = new Date(Date.parse(data.release_date))
+        const dateData = `${releaseDateObject.toLocaleString('default', { month: 'long' })} ${releaseDateObject.getDate()}, ${releaseDateObject.getFullYear()}`
 
         this.setState({
           averageRating: data.average_rating,
@@ -24,13 +27,24 @@ class DetailView extends Component {
           id: data.id,
           overview: data.overview,
           posterPath: data.poster_path,
-          releaseDate: data.release_date,
-          releaseYear: data.release_date.slice(0, 4),
+          releaseDate: releaseDateObject,
+          dateData: dateData,
           revenue: data.revenue,
           runtime: data.runtime,
-          tagline: data.tagline,
           title: data.title
         })
+      })
+
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.movieId}/videos`)
+      .then(response => response.json())
+      .then(videoObject => {
+        const data = videoObject.videos[0]
+        const isEmbedAvailable = data === [] ? false : true
+        if (isEmbedAvailable) {
+          this.setState({
+            videoUrl: data.key
+          })
+        }
       })
   }
 
@@ -38,21 +52,20 @@ class DetailView extends Component {
     return <>
       <section className="details-container" style={{backgroundImage: `linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.75) 50%, rgba(0,0,0,1) 100%),
 url(${this.state.backdropPath})`}}>
+        <button onClick={() => {this.closeMovie()}}>Close Movie</button>
         <img className="poster" src={this.state.posterPath} alt={`Artwork for the movie ${this.state.title}`}/>
         <div className="text-container">
           <h2 className="title">{`${this.state.title} (${this.state.releaseYear})`}</h2>
           <h3 className="genres">{this.state.genres}</h3>
-          <p className="tagline">{this.state.tagline}</p>
-          <p className="rating">{this.state.averageRating}</p>
           <p className="overview">{this.state.overview}</p>
-          <p className="movie-info">{this.state.runtime}</p>
-          <p className="movie-info">{this.state.releaseDate}</p>
-          <p className="movie-info">{this.state.budget}</p>
-          <p className="movie-info">{this.state.revenue}</p>
+          <p className="movie-info">{this.state.runtime} minutes</p>
+          <p className="movie-info">release date: {this.state.dateData}</p>
+          <p className="movie-info">budget: ${this.state.budget}</p>
+          <p className="movie-info">box office: ${this.state.revenue}</p>
         </div>
+        <p className="rating">{this.state.averageRating}</p>
       </section>
-      <iframe className="video-embed" src="https://www.youtube.com/embed/SUXWAEX2jlg" title="Fight Club Trailer - HD" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <button onClick={() => {this.closeMovie()}}>Close Movie</button>
+      {this.state.videoUrl && <iframe className="video-embed" src={`https://www.youtube.com/embed/${this.state.videoUrl}`} title="Fight Club Trailer - HD" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>}
     </>
   }
 }
