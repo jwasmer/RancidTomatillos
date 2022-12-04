98b43fad61f3fa2ce3d2e1f32039f31a28e247e9
James Wasmer <jameswasmer@gmail.com>
2022-12-03 20:48:19 -0500

    Add back button

--- src/DetailView/DetailView.js
+++ src/DetailView/DetailView.js
@@ -1,6 +1,7 @@
 import { keyboard } from "@testing-library/user-event/dist/keyboard"
 import React, { Component } from "react"
 import "./DetailView.css"
+import backButton from "../assets/back-button.png"
 
 class DetailView extends Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class DetailView extends Component {
         const data = movieObject.movie
         const releaseDateObject = new Date(Date.parse(data.release_date))
         const dateData = `${releaseDateObject.toLocaleString('default', { month: 'long' })} ${releaseDateObject.getDate()}, ${releaseDateObject.getFullYear()}`
+        const dateYear = releaseDateObject.getFullYear()
 
         this.setState({
           averageRating: data.average_rating,
@@ -29,13 +31,14 @@ class DetailView extends Component {
           posterPath: data.poster_path,
           releaseDate: releaseDateObject,
           dateData: dateData,
+          dateYear: dateYear,
           revenue: data.revenue,
           runtime: data.runtime,
           title: data.title
         })
       })
 
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.movieId}/videos`)
+    fetch(`https://rancid-tomatibackllos.herokuapp.com/api/v2/movies/${this.movieId}/videos`)
       .then(response => response.json())
       .then(videoObject => {
         const data = videoObject.videos[0]
@@ -48,14 +51,15 @@ class DetailView extends Component {
       })
   }
 
+
   render() {
     return <>
       <section className="details-container" style={{backgroundImage: `linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.75) 50%, rgba(0,0,0,1) 100%),
 url(${this.state.backdropPath})`}}>
-        <button onClick={() => {this.closeMovie()}}>Close Movie</button>
+        <button className="back-btn" onClick={() => {this.closeMovie()}}><img className="btn-icon" src={backButton} alt="An arrow pointing left"/></button>
         <img className="poster" src={this.state.posterPath} alt={`Artwork for the movie ${this.state.title}`}/>
         <div className="text-container">
-          <h2 className="title">{`${this.state.title} (${this.state.releaseYear})`}</h2>
+          <h2 className="title">{`${this.state.title} (${this.state.dateYear})`}</h2>
           <h3 className="genres">{this.state.genres}</h3>
           <p className="overview">{this.state.overview}</p>
           <p className="movie-info">{this.state.runtime} minutes</p>
