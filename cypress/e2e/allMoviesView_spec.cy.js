8f468e7adbcd4686d2a1f61d67dbbcf00e24a11b
Sam Rice <samsrice@gmail.com>
2022-12-05 17:33:45 -0800

    add first 8 <it> blocks

--- cypress/e2e/allMoviesView_spec.cy.js
+++ cypress/e2e/allMoviesView_spec.cy.js
@@ -7,10 +7,48 @@ describe("All Movies View", () => {
       method: "GET",
       url: "https://rancid-tomatillos.herokuapp.com/api/v2/movies" 
     }, testMovies)
+    cy.visit('http://localhost:3000')
   })
 
-  it("should go to main page", () => {
-    cy.visit('http://localhost:3000')
+  it("should display the site title upon page load", () => {
+    cy.contains("RANCID TOMATILLOS")
+  })
+
+  it("should display 10 Tile components upon page load", () => {
+    cy.get("ul").find("li").should("have.length", 10)
+  })
+
+  it("should render the correct data for the first tile", () => {
+    cy.get("ul").find("li").first().find("div").find("img").invoke("attr", "src").should("eql", "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
+    cy.get("ul").find("li").first().find("p").contains("6.9")
+  })
+
+  it("should render the correct data for the last tile", () => {
+    cy.get("ul").find("li").last().find("div").find("img").invoke("attr", "src").should("eql", "https://image.tmdb.org/t/p/original//sDi6wKgECUjDug2gn4uODSqZ3yC.jpg")
+    cy.get("ul").find("li").last().find("p").contains("4.9")
+  })
+
+  it("should not display the search bar immediately upon page load", () => {
+    cy.get(".input-container").find("i").should("not.have.class", "icon-transition")
+    cy.get(".input-container").find("input").should("not.have.class", "input-transition")
+  })
+
+  it("should display the search bar when the search icon is clicked", () => {
+    cy.get(".input-container").find("i").click()
+      .should("have.class", "icon-transition")
+    cy.get(".input-container").find("input").should("have.class", "input-transition")
+  })
+
+  it("should allow text input into the search bar", () => {
+    cy.get(".input-container").find("i").click()
+    cy.get(".input-container").find("input").type("c")
+      .should("have.value", "c")
+  })
+
+  it("should allow text input into the search bar", () => {
+    cy.get(".input-container").find("i").click()
+    cy.get(".input-container").find("input").type("c")
+      .should("have.value", "c")
   })
 
 })
